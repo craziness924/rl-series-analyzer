2e3701f16c18d8dbc776077bde8642a34f5b1e55
craziness924 <jdcollinson001@gmail.com>
2022-08-29 22:29:02 -0500

    #

--- util/plot_util.py
+++
@@ -1,8 +0,0 @@
-# a point on a plot
-class Point():
-    def __init__(self, x, y, color = None, marker = None, label = None):
-        self.x = x
-        self.y = y
-        self.color = color
-        self.marker = marker
-        self.label = label
