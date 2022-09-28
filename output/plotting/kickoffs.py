f889b3d04aed264c34b5e4e729a1f309f0b19acb
craziness924 <jdcollinson001@gmail.com>
2022-09-27 22:22:44 -0500

    Moved around code in Kickoffs

--- output/plotting/kickoffs.py
+++ output/plotting/kickoffs.py
@@ -8,11 +8,16 @@ from util.point import PiePoint
 
 class KickoffPlot():
     @staticmethod
+    def output_plot(series: Series, save_points: bool) -> None:
+        KickoffPlot.plot_first_touches(series=series, save_points=save_points)
+        KickoffPlot.plot_winners()
+
+    @staticmethod
     def get_debug_filename():
         return "kickoff_data.txt"
 
     @staticmethod
-    def determine_winners():
+    def determine_winners(event: Kickoff):
         """
         At some point, we want to make an intelligent detection for a kickoff "win". It's not just about sending ball somewhere.
         
@@ -20,7 +25,47 @@ class KickoffPlot():
         raise NotImplementedError()
 
     @staticmethod
-    def output_plot(series: Series, save_points: bool) -> None:
+    def save_points(filename: str, points) -> None:
+        with open(filename, mode="w") as f:
+            for point in points:
+                f.write(f"{str(point)}\n")
+
+    @staticmethod
+    def plot_winners(series: Series, save_points: bool) -> None:
+        series_winners = []
+        for i, game in enumerate(series.games, 1):
+            game_winners = []
+            for event in game.events:
+                if isinstance(event, Kickoff):
+                    first_touch = KickoffPlot.determine_winners(event)
+
+                    color = "magenta"
+                    explode = 0.0
+
+                    if first_touch == 0:
+                        color = "orange"
+                    elif first_touch == 1:
+                        color = "blue"
+                        
+                    point = PiePoint(event.first_touch_team, color=color)
+
+                    game_winners.append(point)
+                    series_winners.append(point)
+
+            plt = KickoffPlot._create_plot(game_winners, series, i)
+            plt.savefig(f"{game.path}\\kickoff_winners.png")
+
+            if (save_points):
+                save_points(f"{series.series_path}\\game{i}\\{KickoffPlot.get_debug_filename()}", game_winners)
+
+        plt = KickoffPlot._create_plot(series_winners, series)
+        plt.savefig(f"{series.series_path}\\kickoff_winners.png")
+
+        if (save_points):
+            KickoffPlot.save_points(f"{series.series_path}\\{KickoffPlot.get_debug_filename()}", series_winners)
+
+    @staticmethod
+    def plot_first_touches(series: Series, save_points: bool) -> None:
         series_winners = []
         for i, game in enumerate(series.games, 1):
             game_winners = []
@@ -45,18 +90,13 @@ class KickoffPlot():
             plt.savefig(f"{game.path}\\first_touches.png")
 
             if (save_points):
-                with open(f"{series.series_path}\\game{i}\\{KickoffPlot.get_debug_filename()}", mode="w") as f:
-                    for point in game_winners:
-                        f.write(f"{str(point)}\n")
+                save_points(f"{series.series_path}\\game{i}\\{KickoffPlot.get_debug_filename()}", game_winners)
 
         plt = KickoffPlot._create_plot(series_winners, series)
         plt.savefig(f"{series.series_path}\\first_touches.png")
 
         if (save_points):
-            with open(f"{series.series_path}\\{KickoffPlot.get_debug_filename()}", mode="w") as f:
-                for point in series_winners:
-                    f.write(f"{str(point)}\n")
-
+            KickoffPlot.save_points(f"{series.series_path}\\{KickoffPlot.get_debug_filename()}", series_winners)
 
     @staticmethod
     def _create_plot(data: 'list[PiePoint]', series: Series = None, game_num: int = None):
